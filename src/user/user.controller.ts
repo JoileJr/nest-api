c4e56791a9ef5ed82b16d92ddc8d686aae1e7f2a
Márcio Carvalho <m.carvalho@nextage.com.br>
2024-01-08 14:16:10 -0300

    validação com DTO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,61 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from "@nestjs/common";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { UpdatePutUserDto } from "./dto/update-put-user.dto";
+import { UpdatePatchUserDto } from "./dto/update-patch-user.dto";
 
 @Controller('users')
 export class UserController{
 
     @Post()
-    async create(@Body() body){
-        return {body}
+    async create(@Body() {email, name, password}: CreateUserDTO){
+        return {
+            email, 
+            name, 
+            password
+        }  
+    }
+
+    @Get()
+    async list(){
+        return {
+            users:[]
+        }
     }
 
     @Get(':id')
-    async read(@Param() id){
-        return {users:[], id}
+    async show(@Param('id', ParseIntPipe) id: number){
+        return {
+            user:{}, id
+        }
+    }
+
+    @Put(':id')
+    async update(@Body() {email, name, password}: UpdatePutUserDto, @Param('id', ParseIntPipe) id: number){
+        return {
+            method: 'put',
+            email,
+            name,
+            password,
+            id
+        }
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() {email, name, password}: UpdatePatchUserDto, @Param('id', ParseIntPipe) id: number){
+        return {
+            method: 'patch',
+            email,
+            name,
+            password,
+            id
+        }
+    }
+
+    @Delete(':id')
+    async delete(@Param('id', ParseIntPipe) id: number){
+        return {
+            id
+        }
     }
 
 }
